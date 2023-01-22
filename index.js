6a3e31da90ed13719e0ae05a8b3afed3c557b6f6
Smit <smitsekhadia99@gmail.com>
2023-01-23 02:04:14 +0530

    syncing all the peers part of the blockchain when new block is added done

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const app = express();
 const Blockchain = require("./blockchain");
 const bodyParser = require("body-parser");
-const app = express();
 const blockchain = new Blockchain();
+
 const PubSub = require("./publishsubscribe.js");
 const pubsub = new PubSub({ blockchain });
+
+const request = require("request");
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 app.use(bodyParser.json());
@@ -21,7 +26,21 @@ app.post("/api/mine", (req, res) => {
   pubsub.broadcastChain();
   res.redirect("/api/blocks");
 });
-const DEFAULT_PORT = 3000;
+
+// to sync the new server with other peers
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        console.log("Replace chain on sync with ", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
+
 //to run get blockchain on other devicse you need to run on different server so peerport
 let PEER_PORT;
 if (process.env.GENERATE_PEER_PORT === "true") {
@@ -30,4 +49,5 @@ if (process.env.GENERATE_PEER_PORT === "true") {
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`listening to PORT:${PORT}`);
+  syncChains();
 });
